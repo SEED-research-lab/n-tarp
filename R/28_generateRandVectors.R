44761baf3c6f01e5f51531571634b3ec53a77dd0
Taylor Williams <will1630@purdue.edu>
2021-08-02 13:39:37 -0500

    workaround for buggy file import process

--- R/28_generateRandVectors.R
+++ R/28_generateRandVectors.R
@@ -3,7 +3,7 @@
 # Project:      n-TARP clustering
 #               https://github.com/SEED-research-lab/n-tarp
 # 
-# Copyright 2017-2020 Taylor Williams
+# Copyright 2017-2021 Taylor Williams
 # 
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
@@ -22,65 +22,86 @@
 # Authors:      Taylor Williams
 # Affiliation:  Purdue University
 # 
+# Input stack:
+#               CLEAN PROBABILITY MATRIX (feature vector) file
+
+#
 # Description:  
 # 
-# Package dependancies: 
+# Package dependencies: 
 #
 # Changelog:
 #     2019.09.13. forked from other SEED lab projects
 #     2020.02.05. allow user to choose number of random vectors
+#     2021.07.30. Continuing effort to save progress every thousand vectors
+#     2021.07.2x. commented out the file loading code--a bug with grepl is suspected.  Workaround: manually load the 3 RData files
 #                   
-# Feature wishlist:  (*: planned but not complete)
-#     *              
+# Feature wishlist:  (*: planned; /: started; x: complete)
+#     [*] re-enable file loading
 ## ===================================================== ##
 
 
-## Clean the environment ########## 
+
+######### Clean the environment ########## 
+# rm(list=ls())
+# ## Clean the environment except required variables
 varsToRetain <- c("filenameFV", "dataFolderPath")
 rm(list=setdiff(ls(), varsToRetain))
 
-## Required libraries ########## 
-require("readr")
-require("tcltk")
-require("tidyr")
-require("dplyr")
-require("tibble")
-require("beepr")
-
-#Read data from files ####
-## Check for pre-defined starting directory and course prefix ####
-if(!exists("filenamePrefix")) filenamePrefix <- NULL
-if(!exists("dataFolderPath")) dataFolderPath <- NULL
-if(!exists("filenameFV")) filenameFV <- NULL
+######### Internal functions ########## 
 
 
-## get data file locations from user ####
-#Locate the CLEAN probability matrix (feature vector) file
-if(!exists("filenameFV")){
-  #read the CLEAN probability matrix (feature vector) file
-  prompt <- "*****Select the CLEAN PROBABILITY MATRIX (feature vector) file*****\n    (The file picker window may have opened in the background.  Check behind this window if you do not see it.)\n"
-  cat("\n", prompt)
-  filenameFV <- tcltk::tk_choose.files(caption = prompt,
-                                       default = file.path(getwd(), "output", ""),
-                                       filter = matrix(c("CSV", ".csv",
-                                                         "RData", ".RData",
-                                                         "All files", ".*"),
-                                                       3, 2, byrow = TRUE),
-                                       multi = FALSE)
-}
-  
-#load in the data based on the type of data file provided
-if(grepl(x = filenameFV, pattern = "\\.RData$")){
-  load(file = filenameFV)
-  probMatrix <- stu_LO_FV
-}else if(grepl(x = filenameFV, pattern = "\\.(csv|CSV)$")){
-  probMatrix <- read_csv(file = filenameFV)
-}else {
-  message("Invalid Data Filetype.")
-  break
-}
+
+######### Setup ##########
+#load required packages
+require(tidyverse)
+require(readxl)
+require(beepr)
+
+#Load functions 
 
 
+######### Read Data ##########
+
+## WORKAROUND
+probMatrix <- stu_LO_FV #run this and insert the appropriate clean feature vector (aka, probability matrix)
+ 
+# ## Check for pre-defined starting directory and course prefix ####
+# if(!exists("filenamePrefix")) filenamePrefix <- NULL
+# if(!exists("dataFolderPath")) dataFolderPath <- NULL
+# if(!exists("filenameFV")) filenameFV <- NULL
+# 
+# 
+# ## get data file locations from user ####
+# #Locate the CLEAN probability matrix (feature vector) file
+# if(!exists("filenameFV")){
+#   #read the CLEAN probability matrix (feature vector) file
+#   prompt <- "*****Select the CLEAN PROBABILITY MATRIX (feature vector) file*****\n    (The file picker window may have opened in the background.  Check behind this window if you do not see it.)\n"
+#   cat("\n", prompt)
+#   filenameFV <- tcltk::tk_choose.files(caption = prompt,
+#                                        default = file.path(getwd(), "output", ""),
+#                                        filter = matrix(c("CSV", ".csv",
+#                                                          "RData", ".RData",
+#                                                          "All files", ".*"),
+#                                                        3, 2, byrow = TRUE),
+#                                        multi = FALSE)
+# }
+#   
+# #load in the data based on the type of data file provided
+# if(grepl(x = filenameFV, pattern = "\\.RData$")){
+#   load(file = filenameFV)
+#   probMatrix <- stu_LO_FV
+# }else if(grepl(x = filenameFV, pattern = "\\.(csv|CSV)$")){
+#   probMatrix <- read_csv(file = filenameFV)
+# }else {
+#   message("Invalid Data Filetype.")
+#   break
+# }
+
+
+
+
+######### Main ##########
 
 
 ##Generate random vectors ####
